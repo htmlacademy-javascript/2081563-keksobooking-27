40630055b56c7223cad2183713ee8dc37f6e7340
Alexander <a.klivets95@gmail.com>
2022-10-07 19:47:39 +0300

    Исправляет название функций

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomNumber(min, max, round = 1) {
+const getRandomNumber = (min, max, round = 1) => {
   let result;
 
   if (min < 0 || max < 0 || round < 0) {
@@ -12,7 +12,7 @@ function getRandomNumber(min, max, round = 1) {
   result = (Math.random() * (max - min) + min).toFixed(round);
 
   return +result;
-}
+};
 
 const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const TIME = ['12:00', '13:00', '14:00'];
@@ -20,15 +20,15 @@ const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
 const COPY_OBJECT_COUNT = 10;
 
-const GET_AVATAR = (index) => String(index + 1).padStart(2, '0');
-const SHUFFLE_ARRAY = (array) => array.sort( () => .5 - Math.random());
+const getAvatar = (index) => String(index + 1).padStart(2, '0');
+const shuffleArray = (array) => array.sort( () => .5 - Math.random());
 
-const CREATE_OBJECT = (el, index) => {
+const createObject = (el, index) => {
   const LAT = getRandomNumber(35.65000, 35.70000, 5);
   const LNG = getRandomNumber(139.70000, 139.80000, 5);
   return {
     author: {
-      avatar: `img/avatars/user${GET_AVATAR(index)}.png`
+      avatar: `img/avatars/user${getAvatar(index)}.png`
     },
     offer: {
       title: 'Обратите внимание!',
@@ -39,9 +39,9 @@ const CREATE_OBJECT = (el, index) => {
       guests: getRandomNumber(1, 3, 0),
       checkin: TIME[getRandomNumber(0,2,0)],
       checkout: TIME[getRandomNumber(0,2,0)],
-      features: SHUFFLE_ARRAY(FEATURES).slice(0, getRandomNumber(0, FEATURES.length - 1, 0)),
+      features: shuffleArray(FEATURES).slice(0, getRandomNumber(0, FEATURES.length - 1, 0)),
       description: 'Милая уютная квартира на окраине города',
-      photos: SHUFFLE_ARRAY(PHOTOS).slice(0, getRandomNumber(0, PHOTOS.length - 1, 0)),
+      photos: shuffleArray(PHOTOS).slice(0, getRandomNumber(0, PHOTOS.length - 1, 0)),
     },
     location: {
       lat: LAT,
@@ -50,7 +50,8 @@ const CREATE_OBJECT = (el, index) => {
   };
 };
 
-const CREATE_ARRAY = Array.from({length: COPY_OBJECT_COUNT}, CREATE_OBJECT);
+const CREATE_ARRAY = Array.from({length: COPY_OBJECT_COUNT}, createObject);
 
 /* eslint-disable */
 CREATE_ARRAY;
+
