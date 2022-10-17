d0d2769d5191ba97da709677f116c24d3cf5be12
Alexander <a.klivets95@gmail.com>
2022-10-17 09:03:24 +0300

    Добовляет исправления и проверку на наличие описания

--- js/generation.js
+++ js/generation.js
@@ -13,24 +13,39 @@ const MAP_CANVAS = document.querySelector('.map__canvas');
 const NEW_FRAGMENT = document.createDocumentFragment();
 
 GENERATION_MOCKS.forEach((mock) => {
-  const COPY_ELEMENT = POPUP.cloneNode(true);
-  COPY_ELEMENT.querySelector('.popup__title').textContent = mock.offer.title;
-  COPY_ELEMENT.querySelector('.popup__text--address').textContent = mock.offer.address;
-  COPY_ELEMENT.querySelector('.popup__text--price').textContent = `${mock.offer.price} ₽/ночь`;
-  COPY_ELEMENT.querySelector('.popup__type').textContent = TYPE_ROOMS[mock.offer.type];
-  COPY_ELEMENT.querySelector('.popup__text--capacity').textContent = `${mock.offer.rooms} комнаты для ${mock.offer.guests} гостей`;
-  COPY_ELEMENT.querySelector('.popup__text--time').textContent = `Заезд после ${mock.offer.checkin}, выезд до ${mock.offer.checkout}`;
-  const FEATURES_LISTS = COPY_ELEMENT.querySelectorAll('.popup__feature');
+  const COPY_POPUP = POPUP.cloneNode(true);
+  COPY_POPUP.querySelector('.popup__title').textContent = mock.offer.title;
+  COPY_POPUP.querySelector('.popup__text--address').textContent = mock.offer.address;
+  COPY_POPUP.querySelector('.popup__text--price').textContent = `${mock.offer.price} ₽/ночь`;
+  COPY_POPUP.querySelector('.popup__type').textContent = TYPE_ROOMS[mock.offer.type];
+  COPY_POPUP.querySelector('.popup__text--capacity').textContent = `${mock.offer.rooms} комнаты для ${mock.offer.guests} гостей`;
+  COPY_POPUP.querySelector('.popup__text--time').textContent = `Заезд после ${mock.offer.checkin}, выезд до ${mock.offer.checkout}`;
+  const FEATURES_LISTS = COPY_POPUP.querySelectorAll('.popup__feature');
   const MODIFIERS = mock.offer.features.map((el) => `popup__feature--${el}`);
   FEATURES_LISTS.forEach((featureList) => {
     const MODIFIER = featureList.classList[1];
-    if (MODIFIERS.includes(MODIFIER)) {
+    if (!MODIFIERS.includes(MODIFIER)) {
       featureList.remove();
     }
   });
-  COPY_ELEMENT.querySelector('.popup__description').textContent = mock.offer.description;
-  COPY_ELEMENT.querySelector('.popup__avatar').src = mock.author.avatar;
-  const PHOTO_CONTAINER = COPY_ELEMENT.querySelector('.popup__photos');
+  // FEATURES_LISTS.forEach((featuresList) => {
+  //   const itHave = mock.offer.features.some(
+  //     (el) => {
+  //       featuresList.classList.contains(`popup__feature--${el}`);
+  //     });
+  //   console.log(itHave);
+  //   if (!itHave) {
+  //     featuresList.remove();
+  //   }
+  // });
+  if (mock.offer.description) {
+    COPY_POPUP.querySelector('.popup__description').textContent = mock.offer.description;
+  }
+  else {
+    COPY_POPUP.querySelector('.popup__description').remove();
+  }
+  COPY_POPUP.querySelector('.popup__avatar').src = mock.author.avatar;
+  const PHOTO_CONTAINER = COPY_POPUP.querySelector('.popup__photos');
   const PHOTO_ITEM = PHOTO_CONTAINER.querySelector('.popup__photo');
   PHOTO_ITEM.remove();
   mock.offer.photos.forEach((el) => {
@@ -38,18 +53,7 @@ GENERATION_MOCKS.forEach((mock) => {
     PHOTO_ITEM_ELEMENT.src = el;
     PHOTO_CONTAINER.append(PHOTO_ITEM_ELEMENT);
   });
-  NEW_FRAGMENT.append(COPY_ELEMENT);
+  NEW_FRAGMENT.append(COPY_POPUP);
 });
 
 MAP_CANVAS.append(NEW_FRAGMENT.children[0]);
-
-// massive mock.offer.features
-// FEATURES_LISTS.forEach((featuresList) => {
-//   const itHave = mock.offer.features.some(
-//     (el) => {featuresList.classList.contains(`popup__feature--${el}`);
-//     });
-//   console.log(itHave);
-//   if (!itHave) {
-//     featuresList.remove();
-//   }
-// });
